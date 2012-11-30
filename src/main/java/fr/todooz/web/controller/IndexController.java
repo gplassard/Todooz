0ec41b7ddc5b4c4115993a776ad29517321348b3
GabrielPlassard <gabriel.plassard@gmail.com>
2012-11-30 12:02:37 +0100

    Affichage des todo ajoutés à la main

--- src/main/java/fr/todooz/web/controller/IndexController.java
+++ src/main/java/fr/todooz/web/controller/IndexController.java
@@ -1,11 +1,15 @@
 package fr.todooz.web.controller;
 
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import fr.todooz.domain.Task;
 import fr.todooz.service.TaskService;
 
 @Controller
@@ -18,5 +22,34 @@ public class IndexController {
 	    model.addAttribute("tasks", taskService.findAll());
 
 	    return "index";
+	  }
+	  
+	  @PostConstruct
+	  public void bootstrap() {
+		  if (taskService.count()==0){
+			  Task tache1 = new Task();
+			  tache1.setDate(new Date());
+			  tache1.setId(1L);
+			  tache1.setTags("bientot,manger");
+			  tache1.setText("On a faim!!!!");
+			  tache1.setTitle("toto");
+			  taskService.save(tache1);
+			  
+			  Task tache2 = new Task();
+			  tache2.setDate(new Date());
+			  tache2.setId(2L);
+			  tache2.setTags("lorem ipsum");
+			  tache2.setText("pfiouuuuuu");
+			  tache2.setTitle("tutu");
+			  taskService.save(tache2);
+			  
+			  Task tache3 = new Task();
+			  tache3.setDate(new Date());
+			  tache3.setId(3L);
+			  tache3.setTags("Java c'est bien!");
+			  tache3.setText("youhou");
+			  tache3.setTitle("ohohoho");
+			  taskService.save(tache3);
+		  }
 	  }
 }
